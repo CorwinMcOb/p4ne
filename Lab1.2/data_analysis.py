1e463c7b662a59a4d90ee4d76b03960e1c0309d8
Oleg Panfilov <CorwinMcOb@gmail.com>
2021-04-20 18:44:58 +0300

    Lab1.2 working wersion

--- Lab1.2/data_analysis.py
+++ Lab1.2/data_analysis.py
@@ -2,8 +2,24 @@ from matplotlib import pyplot
 from openpyxl import load_workbook
 wb = load_workbook('data_analysis_lab.xlsx')
 sheet = wb['Data']
-years = sheet['A'][1:]
-temperature = sheet['C'][1:]
-activity = sheet['D'][1:]
 
-print (years)
+# years = sheet['A'][1:]
+# temperature = sheet['C'][1:]
+# activity = sheet['D'][1:]
+
+def getvalue(x):
+    return x.value
+
+# years = map(getvalue, sheet['A'][1:])
+# temperature = map(getvalue, sheet['C'][1:])
+# activity = map(getvalue, sheet['D'][1:])
+
+years = list(map(getvalue, sheet['A'][1:]))
+temperature = list(map(getvalue, sheet['C'][1:]))
+activity = list(map(getvalue, sheet['D'][1:]))
+
+# print (activity)
+
+pyplot.plot(years, temperature, label="Температура")
+pyplot.plot(years, activity, label="Температура")
+pyplot.show()
