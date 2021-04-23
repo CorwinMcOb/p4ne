e002fd9f2d06020d1c737ea44ca4450f9105cab7
Oleg Panfilov <CorwinMcOb@gmail.com>
2021-04-23 16:45:49 +0300

    Lab1.5 working

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -1,14 +1,16 @@
 import os
 import fnmatch
+import glob
 
 ip_list = []
 
-for file in fnmatch.filter(os.listdir("./config_files/"), "*.txt"):
+# for file in fnmatch.filter(os.listdir("./config_files/"), "*.txt"):
+for file in glob.glob("./config_files/*.txt"):
     # print(file)
     with open(file) as f:
         for line in f:
             if line.find("ip address")==1:
-                ip_list.append(line)
+                ip_list.append(line.replace("ip address", "").strip())
 
 for i in ip_list:
     print(i)
